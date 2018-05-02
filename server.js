ba9c6c9118aeda6f10a621e5bd13e95ab050f903
Ravi Saxena <ravi.saxena234@gmail.com>
2018-05-02 17:43:07 +0530

    fifth

--- server.js
+++ server.js
@@ -11,26 +11,26 @@ app.use(bodyParser.json())
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-// Connecting to the database
-mongoose.connect(dbConfig.url)
-.then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...');
-    process.exit();
-});
+// // Connecting to the database
+// mongoose.connect(dbConfig.url)
+// .then(() => {
+//     console.log("Successfully connected to the database");    
+// }).catch(err => {
+//     console.log('Could not connect to the database. Exiting now...');
+//     process.exit();
+// });
 
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
 });
 
 
-// Require Notes routes
-require('./app/routes/note.routes.js')(app);
+// // Require Notes routes
+// require('./app/routes/note.routes.js')(app);
 
 
 // listen for requests
